2711319f73ca02f983bdd059d437939120ede7a8
JASDA0000 <103983336+JASDA0000@users.noreply.github.com>
2023-11-03 00:39:23 +0700

    64030238 Jasda

---
+++ main/LED.c
@@ -0,0 +1,16 @@
+#include "driver/gpio.h"    // for gpio function
+extern gpio_num_t LED1;
+void LED_Init()
+{
+	gpio_set_direction(LED1, GPIO_MODE_OUTPUT);
+}
+
+void LED_On()
+{
+	gpio_set_level(LED1, 1);
+}
+
+void LED_Off()
+{
+	gpio_set_level(LED1, 0);
+}
